ae8837fc630a5a038d6c63d34b27a382a365207f
yugesh kumar <yugeshkumar1021003@gmail.come>
2025-08-31 15:44:16 +0530

    final

--- smartcontactmanager/src/main/java/com/practice/smartcontactmanager/controller/AdminController.java
+++ smartcontactmanager/src/main/java/com/practice/smartcontactmanager/controller/AdminController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.practice.smartcontactmanager.dao.UserRepository;
 import com.practice.smartcontactmanager.entities.User;
 
+import jakarta.validation.constraints.Email;
+
 @Controller
 public class AdminController {
 
@@ -51,6 +53,20 @@ public class AdminController {
 	    model.addAttribute("email", email); // keep search box filled
 	    return "admin/admin_pannel";  // 👈 your Thymeleaf template name
 	}
+	
+	
+	// delete the contact 
+	
+	@GetMapping("/admin/delete/{email}")
+	public String deleteUser(@PathVariable("email") String email) {
+	    // extra safety: don't allow admin to delete themselves
+	    User user = userRepository.getUserByUserName(email);
+	    if (user != null && !"ROLE_ADMIN".equals(user.getRole())) {
+	        userRepository.delete(user);  // delete user entity
+	    }
+	    return "redirect:/admin/index"; // redirect back to user list
+	}
+
 
 
 }
